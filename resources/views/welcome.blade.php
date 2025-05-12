93e9730812a30d30932ea197ca2b720abd8c9b88
xsamtech <124665689+xsamtech@users.noreply.github.com>
2025-05-12 05:05:05 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,7 +62,7 @@
                     <div class="row">
                         <div class="col-lg-12">
                             <nav class="navbar navbar-expand-lg">
-                                <a class="navbar-brand" href="{{ route('') }}">
+                                <a class="navbar-brand" href="{{ route('home') }}">
                                     <img id="logo" src="{{ asset('assets/img/logo-text-dark-mode.png') }}" alt="Logo">
                                 </a>
                                 <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
